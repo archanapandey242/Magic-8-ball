0ce7eb31040b6dd641a18ab0a3701168b7fcba8c
arc242 <archanapandey242@gmail.com>
2018-02-12 00:13:13 +0530

    second commit

--- app/src/main/java/com/example/user/magic_8_ball/MainActivity.java
+++ app/src/main/java/com/example/user/magic_8_ball/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button;
-        
         button = (Button) findViewById(R.id.button);
         final ImageView image = (ImageView) findViewById(R.id.ball_image);
         final int[] ballArray = {
